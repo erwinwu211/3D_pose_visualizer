8bbaa8c9a6ee2d520e55071168b00dc9eb7a538a
Erwin Wu <wutong1994@hotmail.com>
2021-03-10 04:47:16 +0900

    add bone visualization

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.gridspec as gridspec
 import argparse
 from viz import show3Dpose
-
+import glob
+import cv2
 
 color="#3498db"
 N_JOINT = 17 # total number of joint
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i","--file_dir",type=str,default="./test.txt",help="Enter the path of input pose file.")
-parser.add_argument("-p","--img_dir",type=str,default="",help="Enter the path of input img folder.")
+parser.add_argument("-p","--img_dir",type=str,default="./imgs",help="Enter the path of input img folder.")
 parser.add_argument("-npy","--is_npy",type=bool,default=False,help="Use NPY file input, CSV as default.")
 parser.add_argument("-n","--number",type=int,default=1,help="Number of Pose in one screen at the same time.")
 
@@ -21,26 +22,26 @@ args = parser.parse_args()
 f_dir = args.file_dir
 num = args.number
 
-if not args.img_dir:
-    img_dir = args.img_dir
-
+img_dir = args.img_dir
+img_list = glob.glob(img_dir+'/*.png')
+print(len(img_list))
 if args.is_npy:
     pose_arr = np.load(f_dir)
 else:
     pose_arr = np.loadtxt(f_dir, delimiter=',')
 
 pose_arr = pose_arr.reshape(-1, num ,N_JOINT,3)
-
+print(pose_arr)
 plt.rcParams['figure.figsize'] = (5*num, 5)
 gs = gridspec.GridSpec(1, num) # 1 rows, n columns
 gs.update(wspace=-0.00, hspace=0.05) # set the spacing between axes.
 plt.axis('off')
 
 for i in range(pose_arr.shape[0]):
-    
     show3Dpose(pose_arr[i],num,color,gs)
-    
-
+    frame = cv2.imread(img_list[i])
+    frame = cv2.resize(frame,(512,512))
+    cv2.imshow('frame',frame)
     plt.show()
 
 
--- viz.py
+++ viz.py
@@ -4,29 +4,34 @@ import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.gridspec as gridspec
 
+I   = np.array([0,1,2,0,4,5,0,7,8,9,8,11,12,8,14,15]) # start points
+J   = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]) # end points
 
 def show3Dpose(vals, num, color,gs):
     for n in range(num):
-        x= vals[n,:,0]
-        y= vals[n,:,1]
-        z= vals[n,:,2]
+        # x= vals[n,:,0]
+        # y= vals[n,:,1]
+        # z= vals[n,:,2]
         ax = plt.subplot(gs[n], projection='3d')
-        ax.plot(x, y, z, lw=2, c=color)
+        
+        for i in np.arange(len(I)):
+            x, y, z = [np.array( [vals[n, I[i], j], vals[n, J[i], j]] ) for j in range(3)]
+            ax.plot(x, z, -y, lw=2, c=color)
 
         if False:
             ax.set_xlabel("x")
             ax.set_ylabel("y")
             ax.set_zlabel("z")
 
-        ax.set_xticks([])
-        ax.set_yticks([])
-        ax.set_zticks([])
+        # ax.set_xticks([])
+        # ax.set_yticks([])
+        # ax.set_zticks([])
 
         ax.get_xaxis().set_ticklabels([])
         ax.get_yaxis().set_ticklabels([])
         ax.set_zticklabels([])
 
-        RADIUS = 10 # space around the subject
+        RADIUS = 256 # space around the subject
         ax.set_xlim3d([-RADIUS, RADIUS])
         ax.set_zlim3d([-RADIUS, RADIUS])
         ax.set_ylim3d([-RADIUS, RADIUS])
